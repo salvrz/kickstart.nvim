fe4d9dd0e59b47210f0c2e5f47a20bc025c459c2
Stevie Alvarez <stevie.6strings@gmail.com>
2025-01-23 22:37:07 -0500

    add ranger and lsp

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -259,6 +259,19 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
     },
+  },
+
+
+  {
+    'kevinhwang91/rnvimr', -- { ['do'] = 'make sync' }
+    keys = {
+      {
+        {'<leader>r', ':RnvimrToggle<CR>', desc = "Toggle ranger"},
+      },
+    },
+    config = function()
+      vim.g.rnvimr_ex_enable = 1
+    end,
   },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
