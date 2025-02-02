56ab2820499cf71665b1aab71fa6ce4084353b28
Stevie Alvarez <stevie.6strings@gmail.com>
2025-02-02 17:51:39 -0500

    lua: begin restructuring into modules

--- init.lua
+++ init.lua
@@ -90,81 +90,10 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Cursor config
-vim.opt.guicursor:append { 'n:hor20-Cursor/lCursor' }
-vim.opt.guicursor:append { 'i:ver25-iCursor' }
-
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
--- [[ Setting options ]]
--- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
-
--- Make line numbers default
-vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
-
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
-
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
-
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
-
--- Enable break indent
-vim.opt.breakindent = true
-
--- Save undo history
-vim.opt.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
-
--- Decrease update time
-vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
-vim.opt.timeoutlen = 300
-
--- Required to keep multiple buffers open
-vim.opt.hidden = true
-
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
-
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
--- Show cursor position all the times
-vim.opt.ruler = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+require('custom/general')
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -736,7 +665,7 @@ require('lazy').setup({
         }
       end,
       formatters_by_ft = {
-        lua = { 'stylua' },
+        -- lua = { 'stylua' },
         rust = { "rustfmt", lsp_format = "fallback" },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
---
+++ lua/custom/interface/init.lua
@@ -0,0 +1,3 @@
+-- Cursor config
+vim.opt.guicursor:append { 'n:hor20-Cursor/lCursor' }
+vim.opt.guicursor:append { 'i:ver25-iCursor' }
