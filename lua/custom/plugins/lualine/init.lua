0564aba205881dad2d1ea8653dd5862c2f575d4c
Stevie Alvarez <stevie.6strings@gmail.com>
2025-02-16 22:17:45 -0500

    lualine: init personal config

--- lua/custom/plugins/lualine/init.lua
+++ lua/custom/plugins/lualine/init.lua
@@ -1,6 +1,15 @@
 --
 -- lualine - Statusline configuration
+--  See all options: https://github.com/nvim-lualine/lualine.nvim
 --
+
+local buffers = {
+  'buffers',
+  show_filename_only = false, -- shows shortened relative path
+  hide_filename_extension = true,
+  show_modified_status = true,
+}
+
 return {
   {
     'nvim-lualine/lualine.nvim',
@@ -9,11 +18,18 @@ return {
     },
     config = function()
       require('lualine').setup {
-        -- [mode, git, curr_file, buffers] [search_count, diagnostics, file_encoding, file_type, row:col/location]
         options = {
           --theme = 'gruvbox',
           section_separators = '',
           component_separators = '',
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'filename', 'diff' },
+          lualine_c = { 'diagnostics', 'searchcount' },
+          lualine_x = { buffers, 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
         },
       }
     end,
